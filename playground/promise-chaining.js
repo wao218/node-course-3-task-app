1f28aef0f02f472b734adb7d4b6eb8729a5af0c3
Wesley Osborne <wao218@lehigh.edu>
2020-02-05 21:39:59 -0500

    Explored promise chaining

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose');
 const User = require('../src/models/user');
 
-User.findByIdAndUpdate('5e395254b25ccd53c4e3d496', { age: 1}).then((user) => {
-  console.log(user);
-  return User.countDocuments({ age: 1});
-}).then((result) => {
-  console.log(result);
+// User.findByIdAndUpdate('5e395254b25ccd53c4e3d496', { age: 1}).then((user) => {
+//   console.log(user);
+//   return User.countDocuments({ age: 1});
+// }).then((result) => {
+//   console.log(result);
+// }).catch((e) => {
+//   console.log(e);
+// });
+
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age: age });
+  const count = await User.countDocuments({ age: age });
+  return count;
+}
+
+updateAgeAndCount('5e395254b25ccd53c4e3d496', 2).then((count) => {
+  console.log(count);
 }).catch((e) => {
   console.log(e);
-});
+})
