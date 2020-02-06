ef581c3f7111800a3ed3d6582c7e81bdd0ce1ae3
Wesley Osborne <wao218@lehigh.edu>
2020-02-06 06:09:04 -0500

    Added routes for updating users and tasks

--- src/index.js
+++ src/index.js
@@ -48,6 +48,35 @@ app.get('/users/:id', async (req, res) => {
   }
 });
 
+// Updating an indiviual user
+app.patch('/users/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'email', 'password', 'age'];
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+  if (!isValidOperation) {
+    return res.status(400).send({
+      error: 'Invalid updates!'
+    });
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (e) {
+    res.send(400).send(e);
+  }
+});
+
+
 // Creating a new task
 app.post('/tasks', async (req, res) => {
   const task = new Task(req.body);
@@ -85,6 +114,31 @@ app.get('/tasks/:id', async (req, res) => {
     res.send(task);
   } catch (e) {
     res.send(500).send();
+  }
+});
+
+// Updating an individual task
+app.patch('/tasks/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['description', 'completed'];
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+  if (!isValidOperation) {
+    return res.status(400).send({
+      error: 'Invalid update!'
+    });
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+
+    if (!task) {
+      return res.send(404).send();
+    }
+
+    res.send(task);
+  } catch (e) {
+    res.status(400).send(e);
   }
 });
 
