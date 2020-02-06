f53c4c7931855177298ad1d7aed5bfbb31750e92
Wesley Osborne <wao218@lehigh.edu>
2020-02-06 06:21:30 -0500

    Added delete routes for users and tasks

--- src/index.js
+++ src/index.js
@@ -76,6 +76,20 @@ app.patch('/users/:id', async (req, res) => {
   }
 });
 
+// Deleting a user
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
 
 // Creating a new task
 app.post('/tasks', async (req, res) => {
@@ -139,6 +153,20 @@ app.patch('/tasks/:id', async (req, res) => {
     res.send(task);
   } catch (e) {
     res.status(400).send(e);
+  }
+});
+
+// Delete a task
+app.delete('/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    
+    res.send(task);
+  } catch (e) {
+    res.status(500).send();
   }
 });
 
