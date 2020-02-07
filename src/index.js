e76a437f4ea842ba12f5c9e6819866bddfbe5bb3
Wesley Osborne <wao218@lehigh.edu>
2020-02-07 06:39:49 -0500

    Tested JSON Web Token library

--- src/index.js
+++ src/index.js
@@ -17,16 +17,14 @@ app.listen(port, () => {
 });
 
 
-const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const myFunction = async () => {
-  const password = 'Red12345!';
-  const hashedPassword = await bcrypt.hash(password, 8);
-  console.log(password);
-  console.log(hashedPassword);
+  const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7 days'});
+  console.log(token);
 
-  const isMatch = await bcrypt.compare('red12345!', hashedPassword);
-  console.log(isMatch);
+  const data = jwt.verify(token, 'thisismynewcourse');
+  console.log(data);
 }
 
 myFunction();
