26cfe9822f318d5b82bf961560c1aabb97618171
Wesley Osborne <wao218@lehigh.edu>
2020-02-08 19:17:51 -0500

    Added authentication to all user routes

--- src/routers/user.js
+++ src/routers/user.js
@@ -52,29 +52,13 @@ router.post('/users/logoutAll', auth, async (req, res) => {
   }
 });
 
-// Reading all users
+// Reading an individual user
 router.get('/users/me', auth, async (req, res) => {
   res.send(req.user);
 });
 
-// Reading an individual user
-router.get('/users/:id', async (req, res) => {
-  const _id = req.params.id;
-
-  try {
-    const user = await User.findById(_id);
-    if (!user) {
-      return res.status(404).send();
-    }
-
-    res.send(user);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
 // Updating an indiviual user
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ['name', 'email', 'password', 'age'];
   const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
@@ -86,30 +70,19 @@ router.patch('/users/:id', async (req, res) => {
   }
 
   try {
-    const user = await User.findById(req.params.id);
-    updates.forEach((update) => user[update] = req.body[update]);
-    await user.save();
-
-    if (!user) {
-      return res.status(404).send();
-    }
-
-    res.send(user);
+    updates.forEach((update) => req.user[update] = req.body[update]);
+    await req.user.save();
+    res.send(req.user);
   } catch (e) {
     res.send(400).send(e);
   }
 });
 
 // Deleting a user
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id);
-
-    if (!user) {
-      return res.status(404).send();
-    }
-
-    res.send(user);
+    await req.user.remove();
+    res.send(req.user);
   } catch (e) {
     res.status(500).send();
   }
