d0c8183fdc14c722c3e89ba8deb07decefefb804
Wesley Osborne <wao218@lehigh.edu>
2020-02-10 18:20:21 -0500

    Added user avatar delete route

--- src/routers/user.js
+++ src/routers/user.js
@@ -92,7 +92,6 @@ router.delete('/users/me', auth, async (req, res) => {
 
 // File upload
 const upload = multer({
-  dest: 'avatars',
   limits: {
     fileSize: 1000000
   },
@@ -104,10 +103,24 @@ const upload = multer({
   }
 });
 
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+// Upload user avatar
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+  req.user.avatar = req.file.buffer;
+  await req.user.save();
   res.send();
 }, (error, req, res, next) => {
   res.status(400).send({ error: error.message });
+});
+
+// Delete user avatar
+router.delete('/users/me/avatar', auth, async (req, res) => {
+  try {
+    req.user.avatar = undefined; 
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
 });
 
 module.exports = router;
