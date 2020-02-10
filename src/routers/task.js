1c56fff2ba2b11c54b76b2171d7314fa635fcf49
Wesley Osborne <wao218@lehigh.edu>
2020-02-10 05:47:12 -0500

    Added pagination to task route

--- src/routers/task.js
+++ src/routers/task.js
@@ -20,6 +20,8 @@ router.post('/tasks', auth, async (req, res) => {
 
 
 // Reading all tasks
+// GET /tasks?limit=10
+// GET /tasks?limit=10&skip=10
 router.get('/tasks', auth, async (req, res) => {
   const match = {};
 
@@ -31,7 +33,11 @@ router.get('/tasks', auth, async (req, res) => {
     // const tasks = await Task.find({ owner: req.user._id });
     await req.user.populate({
       path: 'tasks',
-      match
+      match, 
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip)
+      }
     }).execPopulate();
     res.send(req.user.tasks);
   } catch (e) {
