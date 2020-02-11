ffd6d78fd057c08f3f5a582d123af50ed9222f3a
Wesley Osborne <wao218@lehigh.edu>
2020-02-10 20:13:42 -0500

    Added auto-cropping and image formatting for avatar uploads

--- src/index.js
+++ src/index.js
@@ -8,24 +8,6 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const multer = require('multer');
-const upload = multer({
-  dest: 'images',
-  limits: {
-    fileSize: 1000000
-  },
-  fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(doc|docx)$/)) {
-      cb(new Error('Please upload a Word document'));
-    }
-
-    cb(undefined, true);
-  }
-});
-app.post('/upload', upload.single('upload'), (req, res) => {
-  res.send();
-});
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer');
+const sharp = require('sharp');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 const router = new express.Router();
@@ -105,7 +106,8 @@ const upload = multer({
 
 // Upload user avatar
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-  req.user.avatar = req.file.buffer;
+  const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer();
+  req.user.avatar = buffer;
   await req.user.save();
   res.send();
 }, (error, req, res, next) => {
@@ -131,7 +133,7 @@ router.get('/users/:id/avatar', async (req, res) => {
       throw new Error();
     }
 
-    res.set('Content-Type', 'image/jpg');
+    res.set('Content-Type', 'image/png');
     res.send(user.avatar);
   } catch (e) {
     res.status(404).send();
