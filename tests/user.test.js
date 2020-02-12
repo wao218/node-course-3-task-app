3a9bf26a7925d0aba1333190e71883d1992c1ad7
Wesley Osborne <wao218@lehigh.edu>
2020-02-12 06:15:44 -0500

    Added tests for getting user profile routes

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,11 +1,18 @@
 const request = require('supertest');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 const app = require('../src/app');
 const User = require('../src/models/user');
 
+const userOneId = new mongoose.Types.ObjectId();
 const userOne = {
+  _id: userOneId,
   name: 'Andrew',
   email: 'andrew@example.com',
-  password: '56what!!'
+  password: '56what!!',
+  tokens: [{
+    token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
+  }]
 }
 
 beforeEach(async () => {
@@ -33,4 +40,19 @@ test('Should not login nonexistent user', async () => {
     email: userOne.email,
     password: 'someDummyPassword'
   }).expect(400);
+});
+
+test('Should get profile of user', async () => {
+  await request(app)
+    .get('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+     .expect(200);
+});
+
+test('Should not get profile for unauthorized user', async () => {
+  await request(app)
+    .get('/users/me')
+    .send()
+    .expect(401)
 });
