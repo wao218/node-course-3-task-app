476024fa7683c8767452f7a0aed815c2274e3f8d
Wesley Osborne <wao218@lehigh.edu>
2020-03-16 23:07:49 -0400

    corrected status in update task route

--- src/routers/task.js
+++ src/routers/task.js
@@ -86,7 +86,7 @@ router.patch('/tasks/:id', auth, async (req, res) => {
     const task = await Task.findOne({ _id: req.params.id, owner: req.user._id });
 
     if (!task) {
-      return res.send(404).send();
+      return res.status(404).send();
     }
 
     updates.forEach((update) => task[update] = req.body[update]);
